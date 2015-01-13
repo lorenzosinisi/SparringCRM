c7a0450fb5f24989fb2999439f51541fe70c3f9e
lorenzosinisi <lasslo.net@gmail.com>
2015-01-13 12:37:22 +0100

    fixed and styled homepage

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
+  before_action :authenticate_user!
   def index
+  	
+  	@contacts = current_user.contacts.all
+  	@statuses = current_user.statuses.all
+
   end
 end
